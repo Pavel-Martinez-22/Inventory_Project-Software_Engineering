60543d57e6181757c55e3f1b00d207bba52e84f7
Pavel Martinez <pavel.martinez.22@gmail.com>
2025-10-30 16:49:37 -0700

    Updated Sign in bugs

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
-    "https://www.yoursite.com",
+    "https://inventory-project-software-engineering-u00c.onrender.com",
     "http://127.0.0.1:5500",
-    "http://localhost:3500",
-    "http://localhost:3000",
     "http://localhost:3444"
 ];
 
